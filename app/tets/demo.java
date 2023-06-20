2d5671ea8157ccb44dc0321c7eac300940c5e3b9
Iamneo <techadmin@iamneo.ai>
2023-06-20 09:37:57 +0000

    Backup Commit

--- app/tets/demo.java
+++ app/tets/demo.java
@@ -1,1 +1,25 @@
-]
+import java.util.*;
+
+class Demo{
+    public static void main(String[] args) {
+       for(int i = 0; i <= 100; i++) {
+        System.out.print("hello goip")
+       }
+
+       for(int i = 0; i <= 100; i++) {
+        System.out.print("hello goip")
+       }
+
+       for(int i = 0; i <= 100; i++) {
+        System.out.print("hello goip")
+       }
+
+       for(int i = 0; i <= 100; i++) {
+        System.out.print("hello goip")
+       }
+
+       for(int i = 0; i <= 100; i++) {
+        System.out.print("hello goip")
+       }
+    }
+}
