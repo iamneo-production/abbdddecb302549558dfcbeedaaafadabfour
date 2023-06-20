eb9fa7cadfaf884fc881d316516c881ce9cbf0a2
Iamneo <techadmin@iamneo.ai>
2023-06-20 08:51:41 +0000

    Backup Commit

--- test/demo.java
+++ test/demo.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 class Demo{
     public static void main(String[] args) {
-        System.out.print("sunil");
+       for(int i = 0; i <= 100; i++) {
+        System.out.print("hello goip")
+       }
     }
 }
